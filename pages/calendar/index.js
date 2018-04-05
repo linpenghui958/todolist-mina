d2012044ee42c1a9efb4a805adc77b13b78dd375
linph <370345882@qq.com>
2018-04-05 23:23:59 +0800

    add backhome-btn

--- pages/calendar/index.js
+++ pages/calendar/index.js
@@ -11,6 +11,13 @@ const conf = {
 			// placeholder: '请选择日期', // input 输入框placeholder值
 			// type: 'normal', // [normal 普通单选模式(默认), timearea 时间段选择模式(待开发), multiSelect 多选模式(待完善)]
 		});
+	},
+
+	backHome: function () {
+		console.log('back')
+		wx.navigateBack({
+			delta: 1
+		})
 	}
 };
 Page(conf);
